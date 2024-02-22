dda8eddd4ef3f7a16cbd08e07a43a61b80ab8ff1
celesthaku <unicoarturo@hotmail.com>
2024-02-21 20:24:54 -0500

    21feb2023

--- nuevo_blog/configuracion/base.py
+++ nuevo_blog/configuracion/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'c4zwmw=43alkv*^x@c2r@lh+a3o+9jiulkp4$7))!)-xxa$b2g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['200.121.195.50']
+ALLOWED_HOSTS = []
 
 
 # Application definition
